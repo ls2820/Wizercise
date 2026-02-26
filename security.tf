25c9c1e1a50817b89c3e0f4cd3894650d0c55b40
ls2820 <76626763+ls2820@users.noreply.github.com>
2026-02-26 13:47:16 +0100

    Update S3 bucket name and add public access block

--- security.tf
+++ security.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "audit_log_bucket" {
-..bucket = "wiz-app-audit-logs"
-..# This is a dummy file to ensure the IaC security pipeline has code to scan
+  bucket = "wiz-app-audit-logs-ls2820-unique"
+}
+
+resource "aws_s3_bucket_public_access_block" "bad_example" {
+  bucket                  = aws_s3_bucket.audit_log_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
